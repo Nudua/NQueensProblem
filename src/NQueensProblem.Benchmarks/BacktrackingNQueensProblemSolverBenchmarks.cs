c0cd822bb54b1bcc00a10780b5ccb3d2a0d3a140
Tor Ramstad <tor.audun@gmail.com>
2022-06-05 20:05:46 +0200

    Add benchmarks (#3)
    * Add Benchmarks project
    * Add Benchmarks for BacktrackingNQueensProblemSolver
    * Add benchmark option to the CLI

---
+++ src/NQueensProblem.Benchmarks/BacktrackingNQueensProblemSolverBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace NQueensProblem.Benchmarks;
+
+[MemoryDiagnoser]
+public class BacktrackingNQueensProblemSolverBenchmarks
+{
+    [Benchmark]
+    public List<int[]> BacktrackingEightQueensBenchmark()
+    {
+        var eightQueensSolver = new BacktrackingNQueensProblemSolver(8);
+        return eightQueensSolver.Solve();
+    }
+
+    [Benchmark]
+    public List<int[]> BacktrackingFiveQueensBenchmark()
+    {
+        var fiveQueensSolver = new BacktrackingNQueensProblemSolver(5);
+        return fiveQueensSolver.Solve();
+    }
+
+    [Benchmark]
+    public List<int[]> BacktrackingThreeQueensBenchmark()
+    {
+        var threeQueensSolver = new BacktrackingNQueensProblemSolver(3);
+        return threeQueensSolver.Solve();
+    }
+
+    // TODO: Benchmark other implementations to compare them
+}
--- src/NQueensProblem.CLI/Program.cs
+++ src/NQueensProblem.CLI/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BenchmarkDotNet.Running;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NQueensProblem.Abstractions;
+using NQueensProblem.Benchmarks;
 using NQueensProblem.Factory;
 
 // Setup DI and logging
@@ -16,6 +18,12 @@ var serviceProvider = new ServiceCollection()
 
 switch (args.Length)
 {
+    case 1 when args[0] == "-b" || args[0] == "--benchmark":
+        {
+            Console.WriteLine("Starting benchmark...");
+            BenchmarkRunner.Run<BacktrackingNQueensProblemSolverBenchmarks>();
+        }
+        break;
     case 2 when (args[0] == "-s" || args[0] == "--solve") && int.TryParse(args[1], out var numberOfQueens):
         {
             var factory = serviceProvider.GetRequiredService<INQueensProblemSolverFactory>();
@@ -33,10 +41,11 @@ switch (args.Length)
 
 static void PrintUsage()
 {
-    Console.WriteLine("This program solves the N-Queens Problem.");
+    Console.WriteLine("This program solves the N-Queens Problem or benchmarks the algorithm used to solve it.");
     Console.WriteLine("Usage:");
     Console.WriteLine("NQueensProblemSolver.CLI.exe [options]");
     Console.WriteLine("NQueensProblemSolver.CLI.exe");
     Console.WriteLine("Options:");
     Console.WriteLine("  -s|--solve [numberOfQueens]     Solve");
+    Console.WriteLine("  -b|--benchmark                  Benchmark the algorithm used to solve the problem.");
 }
