2752f580e00b86e1e39122dc73ace819ffa0b4c0
Tor Ramstad <tor.audun@gmail.com>
2022-06-05 19:41:38 +0200

    Create NQueensProblem implementation + tests (#1)
    * Target .NET 6 instead of netstandard
    * Create interface and simple backtracking implementation for solving the problem
    * Create factory classes to create new instances of the solver
    * Make internal classes visible to UnitTest/Benchmark project
    * Add data that verifies correctness of the problem solver
    * Create BacktrackingNQueensProblemSolverTests.cs

---
+++ src/NQueensProblem/BacktrackingNQueensProblemSolver.cs
@@ -0,0 +1,82 @@
+﻿using NQueensProblem.Abstractions;
+
+namespace NQueensProblem;
+
+internal class BacktrackingNQueensProblemSolver : INQueensProblemSolver
+{
+    private readonly int _numberOfQueens;
+
+    private readonly int[] _partialSolve;
+    private readonly List<int[]> _solutions = new();
+
+    public BacktrackingNQueensProblemSolver(int numberOfQueens = 8)
+    {
+        if (numberOfQueens < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberOfQueens));
+        }
+
+        _numberOfQueens = numberOfQueens;
+        _partialSolve = new int[_numberOfQueens];
+    }
+
+    private bool CanPlaceQueen(int currentRow, int position)
+    {
+        // Create a slice that consist of the preceeding queen positions up until the position we want to place on
+        var slice = _partialSolve[..currentRow];
+
+        // Interate through the queens we've already placed until our current position
+        for (int i = 1; i <= slice.Length; i++)
+        {
+            int queenPosition = slice[currentRow - i];
+
+            // Check if the preceeding queen is in the same row
+            // Or if she's on a diagonal path from the position that we want to place on
+            if (position == queenPosition
+                || queenPosition == position - i
+                || queenPosition == position + i)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<int[]> Solve()
+    {
+        if (_solutions.Count == 0)
+        {
+            SolveRecursivly();
+        }
+
+        return _solutions;
+    }
+
+    private void SolveRecursivly(int row = 0)
+    {
+        // If we make it here, we've found a solution
+        if (row == _numberOfQueens)
+        {
+            AddSolution();
+            return;
+        }
+
+        // Try every possible solution for the current row
+        for (int column = 0; column < _numberOfQueens; column++)
+        {
+            if (CanPlaceQueen(row, column))
+            {
+                _partialSolve[row] = column;
+                SolveRecursivly(row + 1);
+            }
+        }
+    }
+
+    private void AddSolution()
+    {
+        var copy = new int[_numberOfQueens];
+        _partialSolve.CopyTo(copy, 0);
+        _solutions.Add(copy);
+    }
+}
---
+++ src/NQueensProblem/Factory/NQueensProblemSolverFactory.cs
@@ -0,0 +1,16 @@
+﻿using NQueensProblem.Abstractions;
+
+namespace NQueensProblem.Factory;
+
+public class NQueensProblemSolverFactory : INQueensProblemSolverFactory
+{
+    public INQueensProblemSolver Create(int numberOfQueens = 8)
+    {
+        if (numberOfQueens < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberOfQueens));
+        }
+
+        return new BacktrackingNQueensProblemSolver(numberOfQueens);
+    }
+}
